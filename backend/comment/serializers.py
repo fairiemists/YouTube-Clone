b51d09814706174d015685ff74ef50231437cab8
fairiemists <v.deweyness@gmail.com>
2023-02-06 11:10:14 -0800

    Updated Serializers URLs and Views + Added launch json

--- backend/comment/serializers.py
+++ backend/comment/serializers.py
@@ -5,4 +5,5 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Comment
         fields = ['id', 'video_id', 'text', 'likes', 'dislikes', 'user_id']
+        depth = 1
         
--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.comment_list), 
-    
+    # path('all/', views.comment_list), 
+    path('all/', views.get_all_comments),
 ]
