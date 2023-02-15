24a11d10cb77c2a90f47ba0b4a333842de1cb90b
fairiemists <v.deweyness@gmail.com>
2023-02-14 16:56:21 -0800

    Update Likes-Dislikes Default

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('all/', views.comment_list), 
     path('', views.user_comments),
     path('all/', views.get_all_comments),
     path('<str:videoId>/', views.get_all_video_comments), 
-    # path('video/', views.get_all_video_comments),
 ]
